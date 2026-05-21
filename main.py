7cb296436d60ba99a48ba109f3d5f693500a13f2
beauanable <beauanable@gmail.com>
2026-05-21 14:03:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -486,7 +486,7 @@ def fetch_papers_for_search(search_term):
         "filter": "from_publication_date:2024-01-01",
         "search": search_term,
         "sort": "cited_by_count:desc",
-        "per-page": 100,
+        "per-page": 200,
     }
 
     response = requests.get(OPENALEX_URL, params=params)
